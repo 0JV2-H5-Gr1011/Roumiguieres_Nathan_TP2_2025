ae64abe0dbde7fc54fd71b06d6f174c27111b5c3
Nath2504 <nathan2504@icloud.com>
2025-05-08 14:52:06 -0400

    exercice navMesh

---
+++ Assets/Scripts/Ennemi1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ennemi1 : MonoBehaviour
+{
+    
+    [SerializeField] private Transform _cible;
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        BougerAgent();
+    }
+
+    void BougerAgent()
+    {
+        _agent.SetDestination(_cible.position);
+    }
+}
